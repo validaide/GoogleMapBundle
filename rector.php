d9588951f14ca713678e5e07fd59b91d784c7e9a
validaide-mark-bijl <mark.bijl@validaide.com>
2022-12-11 16:42:44 +0100

    #183700340 Upgrade to PHP 8.1: validaide/googlemapbundle - Add support for PHP 8.1 - cleanup [#183700340]

--- rector.php
+++ rector.php
@@ -2,34 +2,28 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
-use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-    $parameters->set(Option::PATHS, [
-//        __DIR__ . '/DependencyInjection',
-//        __DIR__ . '/Form',
-//        __DIR__ . '/Templating',
-//        __DIR__ . '/Twig',
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->importNames();
+    $rectorConfig->importShortClasses();
+    $rectorConfig->paths([
+        __DIR__ . '/DependencyInjection',
+        __DIR__ . '/Form',
+        __DIR__ . '/Templating',
         __DIR__ . '/Tests',
+        __DIR__ . '/Twig',
+        __DIR__ . '/IvoryGoogleMapBundle.php',
     ]);
 
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::PHP_70);
-    $containerConfigurator->import(SetList::PHP_71);
-    $containerConfigurator->import(SetList::PHP_72);
-    $containerConfigurator->import(SetList::PHP_73);
-    $containerConfigurator->import(SetList::PHP_74);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_80);
-
-    // get services (needed for register a single rule)
-    // $services = $containerConfigurator->services();
-
     // register a single rule
-    // $services->set(TypedPropertyRector::class);
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_74,
+        // LevelSetList::UP_TO_PHP_81
+    ]);
 };
